17060ed636d02b173d432ebfddf5c84972da6bff
penguin <penguin586970@gmail.com>
2026-07-24 11:20:44 -0400

    UPD: 26.2

--- src/main/java/com/autologin/ChatUtils.java
+++ src/main/java/com/autologin/ChatUtils.java
@@ -6,6 +6,6 @@ import net.minecraft.network.chat.Component;
 public class ChatUtils {
     public static void sendChatMessage(String message) {
         Minecraft client = Minecraft.getInstance();
-        client.execute(() -> client.gui.getChat().addClientSystemMessage(Component.literal(message)));
+        client.execute(() -> client.gui.hud.getChat().addClientSystemMessage(Component.literal(message)));
     }
 }
